a99409a5e5dfc8cd5f45136bc83cc8e8bb610617
NastyaShykhantsova <NastyaShykhantsova@users.noreply.github.com>
2019-04-05 14:47:08 +0300

    Figure

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,9 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Paint");
+        primaryStage.setScene(new Scene(root, 1000, 1000));
+        primaryStage.setResizable(false); // нельзя растягивать форму
         primaryStage.show();
     }
 
---
+++ src/sample/Objects/Figure.java
@@ -0,0 +1,27 @@
+package sample.Objects;
+
+import javafx.scene.canvas.Canvas;
+import sample.Point;
+
+public abstract class Figure {
+
+    public Point fist= new Point();
+    public Point second =new Point();
+    public abstract   void Draw(Canvas canvas);
+    public void   swap()
+    {
+        double temp;
+        if (second.y<fist.y) {
+            temp =second.y;
+            second.y=fist.y;
+            fist.y=temp;
+        }
+        if (second.x<fist.x) {
+            temp =second.x;
+            second.x=fist.x;
+            fist.x=temp;
+
+        }
+    }
+    public  abstract Figure factor();
+}
