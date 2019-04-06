10853d19854c7e72b10a7beebfe97e6737ced759
NastyaShykhantsova <NastyaShykhantsova@users.noreply.github.com>
2019-04-06 07:03:14 +0300

    Romb

--- src/sample/Objects/RightArrow.java
+++ src/sample/Objects/RightArrow.java
@@ -7,7 +7,10 @@ public class RightArrow extends Figure {
     public void Draw(Canvas canvas) {
 
              swap();
-        canvas.getGraphicsContext2D().strokeLine((fist.x-second.x)/2, (fist.y-second.y)/2,second.x, second.y);
+        canvas.getGraphicsContext2D().strokeLine(fist.x, fist.y, second.x, second.y);
+        canvas.getGraphicsContext2D().strokeLine(second.x, second.y, fist.x, 2*second.y - fist.y);
+        canvas.getGraphicsContext2D().strokeLine(fist.x, 2*second.y - fist.y, 2*fist.x - second.x, second.y);
+        canvas.getGraphicsContext2D().strokeLine(2*fist.x - second.x, second.y, fist.x, fist.y);
     }
 
     @Override
